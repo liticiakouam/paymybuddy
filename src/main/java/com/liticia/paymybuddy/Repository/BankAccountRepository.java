4e74cffe7fbffb1c2bedd83764397fc211c274a4
liticiakouam <anzeliticia@gmail.com>
2023-03-02 20:41:49 +0100

    module 1: bank account part1

--- src/main/java/com/liticia/paymybuddy/Repository/BankAccountRepository.java
+++ src/main/java/com/liticia/paymybuddy/Repository/BankAccountRepository.java
@@ -4,6 +4,7 @@ import com.liticia.paymybuddy.Entity.BankAccount;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,5 +12,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface BankAccountRepository extends JpaRepository<BankAccount, Integer> {
 
     @Query("UPDATE BankAccount ba SET status = 0 WHERE ba.id LIKE :id")
-    public BankAccount updateByStatus(@RequestParam(value = "id") int id);
+    public BankAccount updateByStatus(@Param(value = "id") int id);
 }
